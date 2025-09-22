967095bd4a1cba4bbd1a529f3ec10ced701b2c81
jdduprey <jduprey@uw.edu>
2025-09-22 13:17:42 -0700

    adjustment to deps

---
+++ rca_echo_tools/constants.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+ZARR_DAY_0 = datetime(2025, 9, 1)
+
+DATA_BUCKET = "s3://ooi-data"
+
+VIZ_BUCKET = "s3://ooi-rca-qaqc-prod"
+
+ECHO_REFDES_LIST = [
+     "CE02SHBP-MJ01C-07-ZPLSCB101",
+     "CE04OSPS-PC01B-05-ZPLSCB102",
+]
+
+SUFFIX = "streamed-zplsc_volume_scattering"
