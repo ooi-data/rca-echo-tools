fb25c21ce2c0b867e05fc16bd782260bfe0c160c
jdduprey <jduprey@uw.edu>
2026-01-15 13:24:14 -0800

    even bigger dep for debugging

--- rca_echo_tools/pipeline.py
+++ rca_echo_tools/pipeline.py
@@ -66,7 +66,7 @@ def run_echo_raw_data_harvest(
         }   
 
         run_deployment(
-            name=f"echo-raw-data-harvest/echo_tools_16vcpu_80gb",
+            name=f"echo-raw-data-harvest/echo_tools_16vcpu_106gb",
             parameters=params,
             flow_run_name=run_name,
             timeout=12,
