d650b433c70ed789849132bcb1fd491f156a0f4a
jdduprey <jduprey@uw.edu>
2026-02-19 09:58:54 -0800

    logging and full title for echogram

--- rca_echo_tools/echogram.py
+++ rca_echo_tools/echogram.py
@@ -1,14 +1,17 @@
 import echopype as ep
+import matplotlib.pyplot as plt
 
 from datetime import datetime, timedelta
 from pathlib import Path
+from prefect import flow
 
 from rca_echo_tools.constants import SUFFIX
-from rca_echo_tools.utils import load_data
+from rca_echo_tools.utils import load_data, restore_logging_for_prefect
 
 
+@flow(log_prints=True)
 def plot_daily_echogram(
-    date: datetime,
+    date: str,
     refdes: str,
     ping_time_bin: str="4s",
     range_bin: str="0.1m",
@@ -19,12 +22,20 @@ def plot_daily_echogram(
     to compute mean volume backscattering strength (MVBS) that result in gridded data at uniform 
     spatial and temporal intervals based on either number of indices or label values (phyiscal units).
     """
+    restore_logging_for_prefect()
+    print(f"---- Launching: daily echogram for {refdes} on {date} with"
+          f" ping_time_bin={ping_time_bin} and range_bin={range_bin} ----")
+
+    dt = datetime.strptime(date, "%Y/%m/%d")
     output_dir = Path("./output")
     output_dir.mkdir(parents=True, exist_ok=True)
 
-    unbinned_ds = load_data(f"{refdes}-{SUFFIX}")
-    unbinned_ds_day = unbinned_ds.sel(ping_time=slice(date, date + timedelta(days=1)))
+    instrument = refdes[-9:]
 
+    unbinned_ds = load_data(f"{refdes}-{SUFFIX}")
+    unbinned_ds_day = unbinned_ds.sel(ping_time=slice(dt, dt + timedelta(days=1)))
+
+    print("Downsampling data with ep commongrid to deal with offset ping nans.")
     # Reduce data based on sample number
     ds_MVBS = ep.commongrid.compute_MVBS(
         unbinned_ds_day, # calibrated Sv dataset
@@ -33,11 +44,18 @@ def plot_daily_echogram(
         range_bin=range_bin,
     )
 
-    ds_MVBS["Sv"].plot(
+    print("Plotting downsampled array.")
+    facet_grid = ds_MVBS["Sv"].plot(
     x="ping_time",
     row="channel",
-    figsize=(15, 7),
-    vmin=-100, 
+    figsize=(14, 7),
+    vmin=-100,
     vmax=-30,
     cmap="jet"
-)
+    )
+
+    channels = ds_MVBS["channel"].values
+    for ax, channel in zip(facet_grid.axes.flat, channels):
+        ax.set_title(channel)
+
+    plt.savefig(f"{str(output_dir)}/{instrument}_{date}.png")
